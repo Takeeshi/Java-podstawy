02c23539af7fe0decabc89ff1c14dabc8d1aadad
Takeeshi <redrediss@gmail.com>
2019-10-23 20:13:24 +0200

    Cwiczenie 02 modul 2

--- src/modul2/Cwiczenie02.java
+++ src/modul2/Cwiczenie02.java
@@ -6,16 +6,16 @@ public class Cwiczenie02 {
     public static void main(String[] args) {
         System.out.println("Wpisz dodatnią liczbę całkowitą");
         Scanner scan = new Scanner(System.in);
-        var N = scan.nextInt();
-        var K = 0;
+        var n = scan.nextInt();
+        var k = 0;
         var sum = 0;
 
-        while (sum < N) {
-            K = K + 2;
-            sum = sum + K;
+        while (sum < n) {
+            k = k + 2;
+            sum = sum + k;
         }
-        K = K - 2;
-        System.out.println("K to: " + K);
+        k = k - 2;
+        System.out.println("K to: " + k);
     }
 }
 
