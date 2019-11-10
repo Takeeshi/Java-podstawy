0e14545c8fe7e7b78d0f2912446de5fa6b45a423
Takeeshi <redrediss@gmail.com>
2019-11-10 11:47:01 +0100

    Cwiczenie 05 modul 2

--- src/modul2/Cwiczenie05.java
+++ src/modul2/Cwiczenie05.java
@@ -8,11 +8,27 @@ public class Cwiczenie05 {
         Scanner scan = new Scanner(System.in);
         var inputNumber = scan.nextInt();
         if (inputNumber % 2 == 1) {
-            for (var i = 0; i <= inputNumber; i++)
-                System.out.println("*".repeat(i));
+            for (var i = 0; i <= inputNumber; i++) {
+                for (var j = 0; j < i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            for (var i = 0; i < inputNumber; i += 2) {
+                for (int j = inputNumber - i; j > 1; j -= 2) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
         } else if (inputNumber % 2 == 0) {
             System.out.println("Wpisales niewłaściwą liczbę.");
         }
+
+
     }
 }
 
